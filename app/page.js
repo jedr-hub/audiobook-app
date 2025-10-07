b0c82c0fcceff1b8ae9f24d56fae47a3e78fc371
jedr-hub <thanawat.chanket@gmial.com>
2025-10-07 19:33:51 +0700

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
-import { db } from "./firebase";
+import { db } from "../firebase";
 
 export default function HomePage() {
   const [audiobooks, setAudiobooks] = useState([]);
